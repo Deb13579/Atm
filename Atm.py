939d1d3ecdace7aa946a977ee233167a2c2dd70f
Deb13579 <74122863+Deb13579@users.noreply.github.com>
2022-07-15 22:21:19 +0800

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,211 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def print(self):
+        print("Hi, you can withdraw stuff from this account")
+
+
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def print(self):
+        print("Hi, you can withdraw stuff from this account")
+
+        
+self.cashWithdrawl()
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    self.cashWithdrawl()
+NameError: name 'self' is not defined
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def print (self):
+            print("Hi, you can withdraw stuff from this account")
+
+            
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def print(self):
+            print("Hi, you can withdraw stuff from this account")
+
+            
+Atm()
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    Atm()
+TypeError: Atm.__init__() missing 4 required positional arguments: 'CashWithdrawl', 'BalanceEnquiry', 'AccountName', and 'AccountCreationDate'
+Atm(CashWithdrawl)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    Atm(CashWithdrawl)
+NameError: name 'CashWithdrawl' is not defined
+Atm(1,1,1,1)
+<__main__.Atm object at 0x000001A230391180>
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def print(self):
+            print("Hi, you can withdraw stuff from this account")
+
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+
+    
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+
+        
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+
+    
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+
+        
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+        
+    def getName(self, AccountName):
+        self.AccountName = AccountName
+
+        
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+        
+    def getName(self, AccountName):
+        self.AccountName = AccountName
+        
+Deb = Atm(0,101010,"Deb","18th July, 2008")
+SyntaxError: invalid syntax
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+        
+    def getName(self, AccountName):
+        self.AccountName = AccountName
+        
+deb = Atm(0,101010,"Deb","18th July, 2008")
+SyntaxError: invalid syntax
+class Atm(object):
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+        
+    def getName(self, AccountName):
+        self.AccountName = AccountName
+        
+Deb = Atm(0,101010,"Deb","18th July, 2008")
+SyntaxError: invalid syntax
+class Atm:
+    def __init__(self, CashWithdrawl, BalanceEnquiry, AccountName, AccountCreationDate):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.AccountName = AccountName
+        self.AccountCreationDate = AccountCreationDate
+    
+    def setValue(self, BalanceEnquiry):
+        self.BalanceEnquiry = Balance
+        
+    def getName(self, AccountName):
+        self.AccountName = AccountName
+
+        
+Deb = Atm("0","101010","Deb","18/07/08")
+
+print(Deb.AccountName())
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    print(Deb.AccountName())
+TypeError: 'str' object is not callable
+deb = Atm(0,101010,"Deb","18/07/08")
+print(Deb.AccountName())
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    print(Deb.AccountName())
+TypeError: 'str' object is not callable
+print(deb.BalanceEnquiry)
+101010
+deb.BalanceEnquiry
+101010
+deb.AccountName
+'Deb'
+Deb.CashWithdrawl
+'0'
